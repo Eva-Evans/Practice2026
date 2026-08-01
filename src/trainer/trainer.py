5c352154da0cd8ac83f7510e232c45c9076e4a1a
Eva-Evans <68923254+Eva-Evans@users.noreply.github.com>
2026-08-01 14:52:01 +0300

    17

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -8,6 +8,7 @@ class Trainer(BaseTrainer):
     """
 
     def __init__(self, model, criterion, optimizer, device, **kwargs):
+        metrics = kwargs.pop('metrics', None)
         super().__init__(model, criterion, optimizer, device, **kwargs)
         self.criterion = criterion
         self.optimizer = optimizer
